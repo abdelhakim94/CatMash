8767ea746334da33e6c5d50b2036cb8ad7beb56d
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-14 22:19:18 +0200

    Feat: create Image entity class
    This class represents the Image entity in the CatmashDatabase model

---
+++ EntityModel/Image.cs
@@ -0,0 +1,9 @@
+namespace Catmash.EntityModel
+{
+    public class Image
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public int Score { get; set; }
+    }
+}
