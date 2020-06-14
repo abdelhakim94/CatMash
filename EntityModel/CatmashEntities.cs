1dae6e4b74745ec78d4d78636ca2a3daf1fc85af
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-14 22:21:37 +0200

    Feat: create CatmashEntities Entity model class
    This class is the entity model of the CatmashDatabase.

---
+++ EntityModel/CatmashEntities.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Catmash.EntityModel
+{
+    public class CatmashEntities : DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+
+        public CatmashEntities(DbContextOptions<CatmashEntities> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Image>()
+                .HasKey(image => image.Id);
+            modelBuilder.Entity<Image>()
+                .Property(image => image.Url)
+                .IsRequired();
+            modelBuilder.Entity<Image>().
+                Property(image => image.Score).
+                IsRequired();
+        }
+    }
+}
