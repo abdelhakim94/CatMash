13a8f8922bf6ee758c40a07d208fc51fe29a034a
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-15 21:13:55 +0200

    Test: add test of CreateAsync
    The test passes.

--- Tests/DataRepositoryTest/CatmashRepositoryTest.cs
+++ Tests/DataRepositoryTest/CatmashRepositoryTest.cs
@@ -9,6 +9,22 @@ namespace Catmash.DataRepositoryTest
 {
     public class DataRepositoryTest : BaseTest
     {
-        
+        public DataRepositoryTest() : base() { }
+
+        [Fact]
+        public async Task CreateAsync_Image()
+        {
+            // Arrange
+            CatmashRepository repository = new CatmashRepository(context);
+            Image toCreate = new Image { Id = "anId", Url = "www.awebsite.com", Score = 1700 };
+
+            // Act
+            Image returned = await repository.CreateAsync(toCreate);
+            Image created = this.context.Images.Where(img => img.Id == "anId").Single();
+
+            // Assert
+            Assert.Equal(toCreate, returned, new ImageComparer());
+            Assert.Equal(toCreate, created, new ImageComparer());
+        }
     }
 }
