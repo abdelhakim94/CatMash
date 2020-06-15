fd6ae80199b4234ad76a655863603bebff3e813d
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-15 20:27:58 +0200

    Test: create test class for CatmashRepository

---
+++ Tests/DataRepositoryTest/CatmashRepositoryTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using System.Threading.Tasks;
+using Catmash.DataRepository;
+using Catmash.Tests.Basic;
+using Catmash.EntityModel;
+using System.Linq;
+
+namespace Catmash.DataRepositoryTest
+{
+    public class DataRepositoryTest : BaseTest
+    {
+        
+    }
+}
