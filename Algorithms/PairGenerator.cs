239134b6a1b7083dc8cd42e2c70e433b3c4f19d7
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-18 18:59:10 +0200

    Refactor: remove redundant patterns in .gitignore
    Folders like bin/, obj, and .vscode are redundant. Groupe them
    under the same pattern in .gitignore.

---
+++ Algorithms/PairGenerator.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Algorithms
+{
+    /// <summary>
+    ///     A generator of indices pairs that mimics a random behavior
+    /// </summary>
+    public class PairGenerator
+    {
+        /// <summary>
+        ///     For N elements, the set of possible pairs has cardinality N*(N-1).
+        ///     Each pair is assigned a number in range [0, N*(N-1)[ and GetPair
+        ///     returns the kth pair of indices. Each index is in range [0, N-1].
+        ///     The indices calculation follows a pattern that mimics randomness
+        ///     enough so as not to have indices that come up often in successive
+        ///     calls.
+        /// </summary>
+        /// <param name="pairNumber"></param>
+        ///     A number in range nbElements*(nbElements-1) that identifies a pair
+        ///     of indices.
+        /// <param name="nbElements"></param>
+        ///     The number elements.
+        /// <returns></returns>
+        public (int, int) GetPair(int pairNumber, int nbElements)
+        {
+            int possiblePairs = nbElements * (nbElements - 1);
+            pairNumber %= possiblePairs;
+
+            int positionInSlice = pairNumber % (nbElements - 1);
+            int newPairNumber = ((pairNumber + (nbElements - 1) * positionInSlice + 1)) % possiblePairs;
+
+            int firstIndex = newPairNumber / (nbElements - 1);
+            int secondIndex = newPairNumber % (nbElements - 1);
+            if (secondIndex >= firstIndex) secondIndex++;
+
+            return (firstIndex, secondIndex);
+        }
+    }
+}
