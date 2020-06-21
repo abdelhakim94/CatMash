364459809e0ced02e54c6e3ff9816da82b1efed8
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-21 21:10:47 +0200

    Feat: define Constants class to hold all constants needed
    This class has public properties instead of static members. This is more
    convenient to use with dependency injection, as we will access these
    constants from an instance of the class provided by the DI countainer
    instead of using the class name.

---
+++ CatmashWeb/API/Constants.cs
@@ -0,0 +1,9 @@
+namespace Catmash.Web
+{
+    public class Constants
+    {
+        public int ScoreWhenWin { get; } = 1;
+        public int ScoreWhenLose { get; } = 0;
+        public int KFactor { get; } = 10;
+    }
+}
