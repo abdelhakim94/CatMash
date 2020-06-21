b524879bb3f840dbed0ee268a679d20dfe15b76a
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-21 14:49:09 +0200

    Feat: take into account Votes property in comparision

--- EntityModel/ImagePropertiesComparer.cs
+++ EntityModel/ImagePropertiesComparer.cs
@@ -11,14 +11,16 @@ namespace Catmash.EntityModel
             else if (x == null || y == null) return false;
             else return ((x.Id.Equals(y.Id))
                 && (x.Url.Equals(y.Url))
-                && (x.Score == y.Score));
+                && (x.Score == y.Score)
+                && (x.Votes == y.Votes));
         }
         public int GetHashCode(Image obj)
         {
             if (obj is null) return 0;
             return obj.Id.Aggregate<char, int>(0, (i, c) => i + c)
                 + obj.Url.Aggregate<char, int>(0, (i, c) => i + c)
-                + (int)obj.Score;
+                + (int)obj.Score
+                + (int)obj.Votes;
         }
     }
 }
