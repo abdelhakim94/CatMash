3d0549ac7561a5fba2787644728b5089f61fe76a
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-22 20:22:55 +0200

    Fix: remove log message for debugging

--- CatmashWeb/wwwroot/js/modules/signalrInit.js
+++ CatmashWeb/wwwroot/js/modules/signalrInit.js
@@ -1,7 +1,6 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/votes").build();
 
 connection.on("UpdateVotes", function (totalVotes) {
-  console.log("inside hub");
   var totalVotesElement = document.getElementById("totalVotes");
   totalVotesElement.innerHTML = totalVotes + " votes 🔥";
 });
