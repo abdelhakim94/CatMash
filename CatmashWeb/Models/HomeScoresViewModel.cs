d6430e63362e46c3edfe214e4549a9c8450fbf1d
Abdelhakim OUBRAHAM <abdel.oubraham@gmail.com>
2020-06-23 22:17:49 +0200

    Feat: define HomeScoresViewModel view model
    This view model holds a collection of all images to be displayed in the
    scores view.

---
+++ CatmashWeb/Models/HomeScoresViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Catmash.EntityModel;
+
+namespace Catmash.Web.Models
+{
+    public class HomeScoresViewModel
+    {
+        public IEnumerable<Image> images;
+    }
+}
